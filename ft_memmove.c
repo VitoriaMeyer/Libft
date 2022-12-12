488095e697593a694ccc886b4f067409106ae805
VitoriaMeyer <vitoria.mey.s@gmail.com>
2022-12-12 12:57:04 +0000

    working

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: vmeyer-s <vmeyer-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 21:27:36 by vmeyer-s          #+#    #+#             */
-/*   Updated: 2022/02/24 12:53:53 by vmeyer-s         ###   ########.fr       */
+/*   Updated: 2022/12/12 12:52:51 by vmeyer-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,23 @@
 /* memmove copies (len) bytes from (src) to (dst) */
 /* the two strings may overlap, the copy is always done anyway */
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char	*dst2;
-	char	*src2;
 	size_t	i;
 
-	i = 0;
-	if (!dst && !src)
-		return (0);
-	if (dst > src)
+	if (!dest && !src && n > 0)
+		return ((void *) 0);
+	if (src < dest)
 	{
-		i = len - 1;
-		dst2 = dst;
-		src2 = (char *)src;
+		i = n;
+		while (i-- > 0)
+			*((char *)dest + i) = *((const char *)src + i);
 	}
-	while (len--)
+	else
 	{
-		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
-		if (dst > src)
-			i--;
-		else
-			i++;
+		i = -1;
+		while (++i < n)
+			*((char *)dest + i) = *((const char *)src + i);
 	}
-	return (dst);
+	return (dest);
 }
